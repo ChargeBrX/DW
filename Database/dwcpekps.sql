36962cec6ac81d36805bdfaf4fdd1ad531056f56
Panuwat Janjinda <panuwat.janj@ku.th>
2024-02-27 20:05:14 +0700

    Create dwcpekps.sql

---
+++ Database/dwcpekps.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 27, 2024 at 02:04 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dwcpekps`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+-- Error reading structure for table dwcpekps.department: #1932 - Table &#039;dwcpekps.department&#039; doesn&#039;t exist in engine
+-- Error reading data for table dwcpekps.department: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dwcpekps`.`department`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faculty`
+--
+-- Error reading structure for table dwcpekps.faculty: #1932 - Table &#039;dwcpekps.faculty&#039; doesn&#039;t exist in engine
+-- Error reading data for table dwcpekps.faculty: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dwcpekps`.`faculty`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gender`
+--
+-- Error reading structure for table dwcpekps.gender: #1932 - Table &#039;dwcpekps.gender&#039; doesn&#039;t exist in engine
+-- Error reading data for table dwcpekps.gender: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dwcpekps`.`gender`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `generetion`
+--
+-- Error reading structure for table dwcpekps.generetion: #1932 - Table &#039;dwcpekps.generetion&#039; doesn&#039;t exist in engine
+-- Error reading data for table dwcpekps.generetion: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dwcpekps`.`generetion`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+-- Error reading structure for table dwcpekps.student: #1932 - Table &#039;dwcpekps.student&#039; doesn&#039;t exist in engine
+-- Error reading data for table dwcpekps.student: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dwcpekps`.`student`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
